b85cb0e55b22f2980bb99d9e4e16f6ac93cc1589
simple0426 <perfect_0426@qq.com>
2019-11-25 22:49:27 +0800

    fix

--- flask_test/app.py
+++ flask_test/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-app.route('/')
+@app.route('/')
 def hello():
     return "hello docker"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
