ae07b2913ffa42e3666cf075a27273ab1b3edb9d
simple0426 <perfect_0426@qq.com>
2019-11-29 00:10:35 +0800

    flask多容器测试

---
+++ flask_redis/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Time    : 2019/11/28 23:54
+# Author  : simple0426
+# Email   : istyle.simple@gmail.com
+# File    : app.py
+# Software: PyCharm
+
+from flask import Flask
+from redis import Redis
+import os
+import socket
+
+app = Flask(__name__)
+redis = Redis(host=os.environ.get('REDIS_HOST', '127.0.0.1'), port=6379)
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return 'hello Container World！I have been seen %s times and my hostname is %s.\n' % \
+           (redis.get('hits'),socket.gethostname())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
